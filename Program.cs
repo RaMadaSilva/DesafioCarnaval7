4bd53d0295e8238a4d536037af19b2622eb1108f
RaMadaSilva <raul01mateia@gmail.com>
2023-02-23 12:15:48 +0100

    montando a recepcao do valor da ordem

--- Program.cs
+++ Program.cs
@@ -3,6 +3,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("O numero de ordem minimo para essa sequencia é 2"); 
+        Console.WriteLine("====================================================");
+        Console.Write("coloca o numero de ordem da sequencia de Fibinnacci: ");
+        int.TryParse(Console.ReadLine(), out int ordem); 
+
+        if (ordem == 0 && ordem == 1)
+        {
+            Console.WriteLine("numero de Ordem invalido! \n O numero de ordem minimo para essa sequencia é 2"); 
+        }
+        var triangulo = MontadorDeTriangulo(ordem); 
+
+            
+    }
+
+    private static string MontadorDeTriangulo(int valor)
+    {
+
     }
 }
